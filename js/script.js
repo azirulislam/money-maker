ca5159fea48dd41fd196593e841314f2182ee172
azirulislam <azirulislam27@gmail.com>
2022-02-26 23:22:01 +0600

    new commit

--- js/script.js
+++ js/script.js
@@ -17,19 +17,42 @@
     const total = totalExpenses.innerText;
     const expensesAmount = parseFloat(total);
     totalExpenses.innerText = foodCostAmount + rentCostAmount + clothCostAmount;
+    const totalcost = foodCostAmount + rentCostAmount + clothCostAmount;
 
     // Remaining balance
     const balanceBtn = document.getElementById('balance');
     const balanceValue = balanceBtn.innerText;
-    const balanceAmount = parseFloat(balanceValue);
-    balanceBtn.innerText = balanceAmount - (foodCostAmount + rentCostAmount + clothCostAmount);
-    
+    const totalBalance = parseFloat(balanceValue);
+    balanceBtn.innerText = totalBalance  - totalcost;
+     if (totalBalance  < totalcost) {
+            alert('You are crossing your earning limit');
+     }
     // clear input value
     foodCost.value = '' ;
     rentCost.value = '' ;
     clothCost.value = '' ;
-
-    
+   
 }); 
 
+document.getElementById('save-btn').addEventListener('click', function(){
+    const savingsInput = document.getElementById('savings-input');
+    const inputValue = savingsInput.value;
+    const inputNumber = parseFloat(inputValue);
+    savingsInput.value = '';
 
+    const savings = document.getElementById('saving-amount');
+    const savingstext = savings.innerText;
+    const savingsAmount = parseFloat(savingstext);
+    savings.innerText = totalSaving(inputNumber);
+   
+    const reBalance = document.getElementById('re-balance');
+    const balanceText = reBalance.innerText;
+    const remainingAmount = parseFloat(balanceText);
+    reBalance.innerText = savingsAmount
+
+
+     function totalSaving (inputNumber){
+        const result = (inputNumber*10000)/100;
+        return result;
+        }
+});
